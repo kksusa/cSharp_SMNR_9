6d7ef2ce5c53281991757dd9bccd2c6a6f1e922a
kksusa <kksusa1@gmail.com>
2022-09-09 16:59:33 +0500

    Задание 2

--- TSK_2/Program.cs
+++ TSK_2/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿int CheckNumbers(string param)
+{
+    while (true)
+    {
+        Console.Write($"{param} ");
+        if (int.TryParse(Console.ReadLine()!, out int number) && number > 0)
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Число введено неправильно.");
+        }
+    }
+}
+
+long SumFromMtoN (int m, int n)
+{
+    if (m == n) return n;
+    return m + SumFromMtoN(m + 1, n);
+}
+
+int m = CheckNumbers("Введите начальное число:");
+int n = CheckNumbers("Введите конечное число:");
+Console.Write($"Сумма диапазона между числами равна {SumFromMtoN(m, n)}.");
